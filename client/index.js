c21e609f1ee6349297fef3c6cd0a40a84cad2374
Aliaksei Siankevich <asenkevich1994@gmail.com>
2022-09-19 12:42:19 +0300

    add backend

--- client/index.js
+++ client/index.js
@@ -22,16 +22,13 @@ class UserInfo {
 
     try {
       console.log(body)
-      const response = await fetch(
-        'https://tranquil-earth-25667.herokuapp.com/getaddress',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-          },
-          body: JSON.stringify(body),
+      const response = await fetch('http://localhost:4000/getaddress', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
         },
-      )
+        body: JSON.stringify(body),
+      })
       const result = await response.json()
       let data = {
         ...result.data.response.GeoObjectCollection.featureMember[0].GeoObject,
